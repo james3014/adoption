74df73027e64072a7635f8bfa811af1ca89fd2a3
james <grantj3014@gmail.com>
2022-08-29 19:36:01 +0100

    added modal and error boundary

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,12 @@
 import { Component } from 'react';
 import { useParams } from 'react-router-dom';
 import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
+import ThemeContext from './ThemeContext';
+import Modal from './Modal';
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
     const res = await fetch(
@@ -13,12 +16,15 @@ class Details extends Component {
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
   render() {
     if (this.state.loading) {
       return <h2>loading … </h2>;
     }
-
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
 
     return (
@@ -27,7 +33,29 @@ class Details extends Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => {
+              return (
+                <button
+                  onClick={this.toggleModal}
+                  style={{ backgroundColor: theme }}
+                >
+                  Adopt {name}
+                </button>
+              );
+            }}
+          </ThemeContext.Consumer>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <a href="https://bit.ly/pet-adopt">Yes</a>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
           <p>{description}</p>
         </div>
       </div>
@@ -37,7 +65,11 @@ class Details extends Component {
 
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;
---
+++ src/ThemeContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const themeContext = createContext(['green', () => {}]);
+
+export default themeContext;
