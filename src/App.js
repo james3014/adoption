8c5e3430c5cc145ba03e6f246810aab70fbef7eb
james <grantj3014@gmail.com>
2022-08-29 14:36:10 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import SearchParams from './SearchParams';
+import Details from './Details';
+
+const App = () => {
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById('root'));
